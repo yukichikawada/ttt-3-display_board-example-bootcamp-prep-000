4f4f4a4a290c64a8d08c20df040665ab2923d87a
yukichi kawada <yukichikawada@gmail.com>
2017-11-01 22:05:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cell = "   "
-  separates = "|"
-  row = [cell, cell, cell].join(separates)
+  
 end
-#
-# border          = "```"
-# row             = "   |   |   \n"
-# horizontal_line = "-----------\n"
